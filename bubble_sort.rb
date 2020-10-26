483265b8bcec8161954c816740fbc1aec8a0814a
Igors78 <oleinikovsigors@gmail.com>
2020-10-26 13:06:29 +0200

    Loop for sorting added

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,12 +1,13 @@
-def bubble_sort_by(array)
-    (array.length - 1).times do |i|
-        (array.length - 1 - i).times do |j|            
-            array[j], array[j+1] = array[j+1], array[j] if array[j] > array[j+1]
-        end
+def bubble_sort(array)
+  (array.length - 1).times do |i|
+    (array.length - 1 - i).times do |j|
+      result = yield array[j], array[j + 1]
+      array[j], array[j + 1] = array[j + 1], array[j] if result.positive?
     end
-    puts array
+  end
+  puts array
 end
 
-x = [5,7,8,9,2,4,3,1]
+x = [5, 7, 8, 9, 2, 4, 3, 1]
 
-bubble_sort_by(x)
+bubble_sort(x)
