505918b8833fb2e73c9877e403168b8a7b3e5710
Lameck1 <lameck72@gmail.com>
2020-10-26 16:01:36 +0300

    Add method bubble_sort_by

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,28 +1,35 @@
-# def bubble_sort(array)
-#   (array.length - 1).times do |i|
-#     (array.length - 1 - i).times do |j|
-#       result = yield array[j], array[j + 1]
-#       array[j], array[j + 1] = array[j + 1], array[j] if result.positive?
-#     end
-#   end
-#   puts array
-# end
+def bubble_sort(arr)
+    loop do
+        swap_took_place = false
+        (arr.length - 1).times do |i|
+            if arr[i] > arr[i + 1]
+            arr[i], arr[i + 1] = arr[i + 1], arr[i]
+            swap_took_place = true
+            end
+        end
+        break if !swap_took_place
+    end
+    p arr
+end
+
+def bubble_sort_by(arr)
+    loop do
+        swap_took_place = false
+        (arr.length - 1).times do |i|
+            swapped = yield arr[i], arr[i+1]
+            arr[i], arr[i + 1] = arr[i + 1], arr[i] if swapped.positive?
+            swap_took_place = true
+        end
+        break if !swap_took_place
+    end
+    p arr
+end
 
 x = [5, 7, 8, 9, 2, 4, 3, 1]
 
-# bubble_sort(x)
-def bubble_sort(arr)
-  swap_took_place = false
-
-  (arr.length - 1).times do |i|
-    if arr[i] > arr[i + 1]
-      arr[i], arr[i + 1] = arr[i + 1], arr[i]
-      swap_took_place = true
-    end
-  end
-  if !swap_took_place
-    break
-  end
+bubble_sort_by(x) do |m, n|
+    m - n
 end
 
-bubble_sort(x)
+
+bubble_sort_by(x)
