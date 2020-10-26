c5f730462f0245932ce4a8427200a582b41848c6
Lameck1 <lameck72@gmail.com>
2020-10-26 17:51:23 +0300

    Fix bubble_sort_by method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -7,28 +7,19 @@ def bubble_sort(arr)
         swap_took_place = true
       end
     end
-    break if !swap_took_place
+    break unless swap_took_place
   end
-  p arr
+  arr
 end
 
 def bubble_sort_by(arr)
   loop do
     swap_took_place = false
     (arr.length - 1).times do |i|
-      if yield (arr[i], arr[i + 1]).positive? arr[i], arr[i + 1] = arr[i + 1], arr[i]
-        swap_took_place = true
-      end
+      arr[i], arr[i + 1] = arr[i + 1], arr[i] if yield(arr[i], arr[i + 1]).positive?
+      swap_took_place = true
     end
-    break if !swap_took_place
+    break unless swap_took_place
   end
-  p arr
+  arr
 end
-
-x = [5, 7, 8, 9, 2, 4, 3, 1]
-
-bubble_sort_by do |a, b|
-  a - b
-end
-
-bubble_sort(x)
